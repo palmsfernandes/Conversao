d1b764ffef6c5ac96ead0b1b3267c549df5ac148
Vinicius Fernandes <vinicius.fernandes@bunzlbrasil.protcapad.com.br>
2020-06-01 18:48:07 -0300

    Conversao

---
+++ Conversao/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+
+            Console.Write("Digite o valor em real para conversão: R$");
+            valor = Convert.ToDouble(Console.ReadLine());
+
+            double conversaoDollar;
+            conversaoDollar = valor / 5;
+
+            double conversaoEuro;
+            conversaoEuro = valor / 6;
+
+            Console.WriteLine("Conversao de Real para Dollar: " + conversaoDollar);
+            Console.WriteLine("Conversao de Real para Euro: " + conversaoEuro);
+
+            Console.ReadLine();
+
+           
+
+
+        }
+    }
+}
